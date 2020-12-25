031c5ae7e08969541f420c1bb5ca4eb2bd4d0ee8
김강태 <kangtae49@gmail.com>
2020-12-25 15:03:25 +0900

    add movies

--- doc.md
+++ doc.md
@@ -38,8 +38,39 @@ git commit -m "init"
 git push origin master
 ```
 
-props validate
+install props validate
 ---
 ```
 npm i prop-types
+```
+
+install axios
+---
+```
+npm i axios
+```
+
+https://yts.mx/api/v2/list_movies.json
+
+https://yts.lt
+
+https://yts-proxy.now.sh/list_movies.json
+
+
+
+
+
+
+
+
+vs shortcut
+---
+```
+ctrl + d
+alt + click            multi cursor
+alt + up/down          move
+alt + shift + up/down  copy
+ctrl + /               comment
+alt + shift + drag     multi cursor
+ctrl + b               side bar
 ```
--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log('constructor');
-  }
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    console.log('add');
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    console.log('minus');
-    this.setState(current => ({count: current.count-1}));
-  };
-
-  componentDidMount() {
-    console.log("componentDidMount");
+  
+  getMovies = async () => {
+    // const movies = await axios.get("https://yts-proxy.new.sh/list_movies.json");
+    const {data:{data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   }
 
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
+  componentDidMount(){
+    this.getMovies();
   }
 
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
 
   render () {
-    console.log('render')
+    const {isLoading, movies} = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ):(
+          <div class="movies">
+          {movies.map(movie => {
+            return <Movie 
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          })}
+          </div>
+        )
+      }
+      </section>
     )
   }
 }
