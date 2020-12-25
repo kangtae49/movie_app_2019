06a788768ecaa6807724b8e70648428317be5e81
김강태 <kangtae49@gmail.com>
2020-12-25 12:40:41 +0900

    tut life cycle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor');
+  }
   state = {
     count: 0
   };
@@ -15,7 +19,20 @@ class App extends React.Component {
     this.setState(current => ({count: current.count-1}));
   };
 
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
   render () {
+    console.log('render')
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
